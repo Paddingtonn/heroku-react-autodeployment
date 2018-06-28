f44d377a680fae5b4b6d8e7de65739b0cce23691
Paddingtonn <pasiut52@gmail.com>
2018-06-28 11:26:20 +0200

    solving style problems

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
                 test: /\.(png|jpg|ico|gif|mp4)$/,
                 loader: 'file-loader',
                 query: {
-                    name: 'assets/img/[name].[ext]'
+                    name: 'images/[hash]-[name].[ext]'
                 }
             },
             {
